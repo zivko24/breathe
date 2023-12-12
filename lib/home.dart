0c67de68c03f309b8e08ede51256fbfafc5ace94
Zivko Popic <zpopic@dyn-eduroam235035.wlan.jku.at>
2023-12-12 18:29:54 +0100

    Add button for start exercise

--- lib/home.dart
+++ lib/home.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:breathe/utils/customTheme.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final ThemeData theme = Theme.of(context);
-    return Card(
-      shadowColor: Colors.transparent,
-      margin: const EdgeInsets.all(8.0),
-      child: SizedBox.expand(
-        child: Center(
-          child: Text(
-            'Home page test',
-            style: theme.textTheme.titleLarge,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.6, // Set the width to 50% of the screen width
+          height: MediaQuery.of(context).size.width * 0.6, // Set the height to 50% of the screen width
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Palette.primaryColor, // Adjust the color as needed
+          ),
+          child: ElevatedButton(
+            onPressed: () {
+              // Function to start exercise
+            },
+            child: Text(
+              'Start Exercise',
+              style: TextStyle(fontSize: 44),
+            ),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Palette.primaryColor,
+              elevation: 0,
+            ),
           ),
         ),
       ),
--- lib/utils/customTheme.dart
+++ lib/utils/customTheme.dart
@@ -4,4 +4,6 @@ class Palette {
   static const primaryColor = Colors.blue;
   static const buttonClicked = Color(0xff35a7e3);
   static const navigationBackgroundColor = Color(0xffe3f2fd);
+  static const textWhite = Color(0xffffffff);
+  static const texBlack = Color(0x00000000);
 }
